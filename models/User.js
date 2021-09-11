ca8bda4022883f16fe29461143c2d0d6fd5ca63d
Erin Holmes <e.holmes09@yahoo.com>
2021-09-11 17:12:59 -0500

    add veirtual to user schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, Types, model } = require("mongoose");
 
 const UserSchema = new Schema (
   {
@@ -14,9 +14,22 @@ const UserSchema = new Schema (
       unique: true,
       match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
     },
-    // thoughts
-    // friends
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: "Thought"
+    }, 
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: User
+    }
   }
 )
 
-module.exports = Pizza;
+// the length of the user's friends array field
+UserSchema.virtual("friendCount").get(function() {
+  return this.friends.length;
+})
+
+const User = model("User", UserSchema);
+
+module.exports = User;
